06c9148e71aec199a5d83136586bcd1bf6842cad
Greg Van Aken <greg.vanaken@mobilionsystems.com>
2020-04-20 22:20:01 -0400

    Fixing typo in setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
-        'LICENSE :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
